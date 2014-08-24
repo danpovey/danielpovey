695a5bab0f113e91a471114ddb65aa815dedb023
Daniel Povey <dpovey@gmail.com>
2014-08-24 00:16:29 +0000

    some minor changes

---
+++ php/test.php
@@ -0,0 +1,100 @@
+<html><body>
+
+<h2>My first PHP page</h2>
+
+<?php
+$color = "red";
+echo "Server is $_SERVER<br />";
+$x = NULL;
+echo "Position is: ";
+echo strpos("hello, world!", "wo");
+$str1 = "hi,"; $str2 = " there";
+$str1 .= $str2;
+echo "<br />";
+echo $str1;
+echo "<br />";
+$y = 100.0;
+$array = array("c","b");
+var_dump($x);
+var_dump($array);
+var_dump($color);
+var_dump($y);
+echo "<br />\n";
+function foo() {
+   echo "In function foo <br />\n";
+}
+$array2 = array("a" => 1, "b" => 2);
+foreach ($GLOBALS as $x) {
+  $k = key($GLOBALS);
+  echo "this element of GLOBALS is $k -> ";
+  var_dump($x); 
+  echo " <br />";
+}
+$c = $GLOBALS['color'];
+echo "The color is $c<br />";
+echo "Listing is: <br />";
+echo `ls -l /`;
+
+foo();
+?>
+
+
+
+
+
+<p> Hello, </p>
+
+<p> Google has been notified, according to the terms of the Digital Millennium Copyright Act (DMCA), that some of your materials allegedly infringe upon the copyrights of others. The details of the allegedly infringing materials may be found at the end of this message. The notice that we received, with any personal contact information (i.e. phone number, e-mail and address) removed, will be posted online by a service called Chilling Effects at http://www.chillingeffects.org. You can search for the notice associated with the removal of your content by going to the Chilling Effects search page at http://www.chillingeffects.org/search.cgi, and entering in the URL in question. Please note that it may take several weeks for the notice to be posted on the above page. For this reason, we have attached a copy of the notice for your reference. </p>
+
+<p> The DMCA is a United States copyright law that provides guidelines for online service provider liability in case of copyright infringement. We are asking that you please remove the allegedly infringing content from your page. If you do not remove the content by the 3rd day from the date of this notice, we will be forced to remove it. If we did not do so, we would be subject to a claim of copyright infringement, regardless of its merits. See http://www.educause.edu/library/digital-millennium-copyright-act-dmca for more information about the DMCA, and see http://www.google.com/support/bin/request.py?contact_type=lr_dmca&product=sites for the process that Google requires in order to make a DMCA complaint.
+
+<p> Please note that repeated violations of our Terms of Service may result in further remedial action taken against your Google account or associated accounts, including suspension or termination.
+
+<p> Google can reinstate these materials upon receipt of a counter notification pursuant to sections 512(g)(2) and (3) of the DMCA. For more information about the requirements of a counter notification, see http://www.google.com/support/bin/request.py?contact_type=lr_counternotice&product=sites 
+
+<p> If you have legal questions about this notification, you should retain your own legal counsel. If you have any other questions about this notification, please let us know.
+
+<p> The affected URLs are listed below:
+<p> https://bc9cdd19-a-62cb3a1a-s-sites.googlegroups.com/site/dpovey/csl10_sgmm_preprint.pdf?attachauth=ANoY7crgE8Jja27_UIt1xax23mv26Y8GkTjo_-mUay6VTRK3j_QIE0ZtFea38pCy1pDFB26zdlS5D88QDmoxPOPcrR2pSikqt1APqWkABc7vx3N3YhYYNKIY3v2-HvwUmWpl5MGxVjOLB5Ba2upN98hgkuqHxmhs1Z_ICnBywEcXUQHouLy-N9nCD5pFuZ1U4_f9vFpXka37Yt0T-O-qAeingV5B39irOw==&attredirects=0 
+
+<p> Regards,
+<p> The Google Team
+
+<p> Text of the received notice:
+
+<p> AutoDetectedBrowser: Google Chrome 
+<p> AutoDetectedOS: Windows 7 
+<p> IIILanguage: en 
+<p> IssueType: lr_dmca 
+<p> Language: en 
+<p> agree1: checked 
+<p> agree: checked 
+<p> companyname: Attributor Coporation 
+<p> country_residence: US 
+<p> description_of_copyrighted_work: Original Work: The subspace Gaussian 
+<p> mixture model - A structured model for speech recognition 
+<p> Infringing URL: 
+<p> https://bc9cdd19-a-62cb3a1a-s-sites.googlegroups.com/site/dpovey/csl10_sgmm_preprint.pdf?attachauth=ANoY7crgE8Jja27_UIt1xax23mv26Y8GkTjo_-mUay6VTRK3j_QIE0ZtFea38pCy1pDFB26zdlS5D88QDmoxPOPcrR2pSikqt1APqWkABc7vx3N3YhYYNKIY3v2-HvwUmWpl5MGxVjOLB5Ba2upN98hgkuqHxmhs1Z_ICnBywEcXUQHouLy-N9nCD5pFuZ1U4_f9vFpXka37Yt0T-O-qAeingV5B39irOw==&attredirects=0 
+
+<p> full_name: Attributor Corporation 
+<p> geolocation: PH 
+<p> hidden_category: 
+<p> hidden_ctx: 
+<p> hidden_product: groups 
+<p> location_of_copyrighted_work: Original Work: The subspace Gaussian mixture 
+<p> model - A structured model for speech recognition 
+<p> Infringing URL: 
+<p> https://bc9cdd19-a-62cb3a1a-s-sites.googlegroups.com/site/dpovey/csl10_sgmm_preprint.pdf?attachauth=ANoY7crgE8Jja27_UIt1xax23mv26Y8GkTjo_-mUay6VTRK3j_QIE0ZtFea38pCy1pDFB26zdlS5D88QDmoxPOPcrR2pSikqt1APqWkABc7vx3N3YhYYNKIY3v2-HvwUmWpl5MGxVjOLB5Ba2upN98hgkuqHxmhs1Z_ICnBywEcXUQHouLy-N9nCD5pFuZ1U4_f9vFpXka37Yt0T-O-qAeingV5B39irOw==&attredirects=0 
+<p> 
+<p> represented_copyright_holder: Reed Elsevier - Journals 
+<p> signature: Attributor Corporation 
+<p> signature_date_day: 14 
+<p> signature_date_month: 2 
+<p> signature_date_year: 2013 
+<p> url_box_1: 
+<p> https://bc9cdd19-a-62cb3a1a-s-sites.googlegroups.com/site/dpovey/csl10_sgmm_preprint.pdf?attachauth=ANoY7crgE8Jja27_UIt1xax23mv26Y8GkTjo_-mUay6VTRK3j_QIE0ZtFea38pCy1pDFB26zdlS5D88QDmoxPOPcrR2pSikqt1APqWkABc7vx3N3YhYYNKIY3v2-HvwUmWpl5MGxVjOLB5Ba2upN98hgkuqHxmhs1Z_ICnBywEcXUQHouLy-N9nCD5pFuZ1U4_f9vFpXka37Yt0T-O-qAeingV5B39irOw==&attredirects=0
+
+
+
+</center>
+</body></html>
